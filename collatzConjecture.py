7194fa8dffab54759ac84b5d0b51323e32fa96d0
Mustapha Ayad <mustapha.ayad8934@gmail.com>
2023-01-17 11:20:44 -0500

    added text

---
+++ collatzConjecture.py
@@ -0,0 +1,9 @@
+c0 = int(input("please enter a number\n"))
+steps = 0
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 //= 2
+    else: c0 = c0*3+1
+    steps += 1
+    print(c0)
+print("steps:", steps)
